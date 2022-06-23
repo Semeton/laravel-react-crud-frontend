8bd1f50aec383f66f7a154e4c265a0d7983928b4
Semeton Balogun <balogunsemeton@gmail.com>
2022-06-23 01:03:41 +0100

    create component

---
+++ src/components/product/create.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+export default function CreateProduct() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [validationError, setValidationError] = useState({});
+}
